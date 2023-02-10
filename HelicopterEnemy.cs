313472c6a45f8ef1e89f5470bc4f66e7a9634e04
Robert Rumney <robertrumney@gmail.com>
2023-02-10 16:47:50 +0200

    Update HelicopterEnemy.cs

--- HelicopterEnemy.cs
+++ HelicopterEnemy.cs
@@ -1,91 +1,91 @@
 using UnityEngine;
 using System.Collections;
 
-public class HelicopterEnemy : MonoBehaviour 
+public class HelicopterEnemy : MonoBehaviour
 {
 	#region Variables
 	// array of audio clips for shots
-	public AudioClip[] shots; 
+	public AudioClip[] shots;
 
 	// array of transforms for guns
-	public Transform[] guns; 
+	public Transform[] guns;
 
 	// transform of target object
-	public Transform target; 
+	public Transform target;
 	// transform of helicopter object
-	public Transform helicopterTransform; 
+	public Transform helicopterTransform;
 
 	// prefab for tracer effect
-	public GameObject tracer; 
+	public GameObject tracer;
 	// prefab for fire effect
-	public GameObject fire; 
+	public GameObject fire;
 	// prefab for explode effect
-	public GameObject explode; 
+	public GameObject explode;
 	// prefab for blades effect
-	public GameObject blades; 
+	public GameObject blades;
 
 	// health value of helicopter
-	public float health = 10000; 
+	public float health = 10000;
 	// speed value for helicopter movement
-	public float speed = 0.1f; 
+	public float speed = 0.1f;
 	// turn speed for helicopter movement
-	public float turnSpeed = 1; 
+	public float turnSpeed = 1;
 	// target height for helicopter
-	public float targetHeight = 0; 
+	public float targetHeight = 0;
 	// current height of helicopter
-	public float currentHeight = 15; 
+	public float currentHeight = 15;
 	// offset value for helicopter movement
-	public float offset = 0; 
+	public float offset = 0;
 	// fire rate for shooting
-	public float fireRate = 0.01f; 
+	public float fireRate = 0.01f;
 	// attack distance for shooting
-	public float attackDistance; 
+	public float attackDistance;
 
 	// flag to check if helicopter is dead
-	private bool dead; 
+	private bool dead;
 	// flag to check if helicopter is falling
-	private bool falling; 
+	private bool falling;
 	// flag to check if helicopter is shooting
-	private bool shooting; 
+	private bool shooting;
 
 	// reference to old collider
-	public BoxCollider oldCollider; 
+	public BoxCollider oldCollider;
 	// reference to new collider
-	public BoxCollider newCollider; 
+	public BoxCollider newCollider;
 
 	// variable to store the raycast hit information
 	private RaycastHit hit;
 	#endregion
 
-    	#region Private Methods
-   	private void Start()
+	#region Private Methods
+	private void Start()
 	{
-	    // Get reference to the target (player's weapon camera)
-	    target = Game.instance.playerScript.weaponCameraObj.transform;
+		// Get reference to the target (player's weapon camera)
+		target = Game.instance.playerScript.weaponCameraObj.transform;
 
-	    // Start coroutines for shooting and updating AI
-	    StartCoroutine (Shoot ());
-	    StartCoroutine (UpdateAI ());
+		// Start coroutines for shooting and updating AI
+		StartCoroutine(Shoot());
+		StartCoroutine(UpdateAI());
 	}
-	
+
 	// Fire weapon at target
 	private void Fire(Vector3 position, Transform origin)
 	{
-	    // Point the origin transform at the target position
-	    origin.LookAt(position);
+		// Point the origin transform at the target position
+		origin.LookAt(position);
 
-	    // Create a tracer game object at the origin position and rotation
-	    GameObject GO = (GameObject)Instantiate(tracer, origin.position, origin.rotation);
-	    Rigidbody instantiatedProjectile = GO.GetComponent<Rigidbody>();
+		// Create a tracer game object at the origin position and rotation
+		GameObject GO = (GameObject)Instantiate(tracer, origin.position, origin.rotation);
+		Rigidbody instantiatedProjectile = GO.GetComponent<Rigidbody>();
 
-	    // Check the player's health and apply damage
-	    if (Game.instance.playerScript.hitPoints > 30)
-		Game.instance.playerScript.PlayerDamage(1);
-	    else
-		Game.instance.playerScript.PlayerDamage(5);
+		// Check the player's health and apply damage
+		if (Game.instance.playerScript.hitPoints > 30)
+			Game.instance.playerScript.PlayerDamage(1);
+		else
+			Game.instance.playerScript.PlayerDamage(5);
 
-	    // Set the velocity of the instantiatedProjectile
-	    instantiatedProjectile.velocity = origin.TransformDirection(new Vector3(0, 0, 500));
+		// Set the velocity of the instantiatedProjectile
+		instantiatedProjectile.velocity = origin.TransformDirection(new Vector3(0, 0, 500));
 	}
 
 	// Handle helicopter death sequence
@@ -94,7 +94,7 @@ public class HelicopterEnemy : MonoBehaviour
 		// Disable the old collider and enable the new collider
 		oldCollider.enabled = false;
 		newCollider.enabled = true;
-		
+
 		// Raycast from the helicopter's position towards the ground
 		RaycastHit hit;
 		int layerMask = 1 << 10; // Only consider objects on layer 10
@@ -132,22 +132,22 @@ public class HelicopterEnemy : MonoBehaviour
 	{
 		StartCoroutine(Falling());
 	}
-	
+
 	// If the helicopter hits the ground while falling before distance threshold : explode
 	private void OnCollisionEnter(Collision collision)
 	{
-	    if(falling)
-	    {
-	        if(collision.gameObject.CompareTag("Terrain"))
-	        {
-	            if(!dead)
-	            {
-		       Die();
-		       dead = true;
-	            }
-	        }
-	    }
-   	}
+		if (falling)
+		{
+			if (collision.gameObject.CompareTag("Terrain"))
+			{
+				if (!dead)
+				{
+					Die();
+					dead = true;
+				}
+			}
+		}
+	}
 	#endregion
 
 	#region Coroutines
@@ -236,7 +236,7 @@ public class HelicopterEnemy : MonoBehaviour
 	private IEnumerator Shoot()
 	{
 		// Wait for 5 seconds before starting the shooting
-		yield return new WaitForSeconds (5);
+		yield return new WaitForSeconds(5);
 
 		// Continuously check for shooting
 		while (true)
@@ -244,105 +244,105 @@ public class HelicopterEnemy : MonoBehaviour
 			if (shooting)
 			{
 				// Calculate the distance between the helicopter and the target
-				float distance = Vector3.Distance (transform.position, target.position);
+				float distance = Vector3.Distance(transform.position, target.position);
 
 				// Randomize the position of the shot's sound effect
 				Vector3 pos = Game.instance.player.transform.position + Random.insideUnitSphere * distance / 40;
 
 				// Choose a random audio clip from the shots array
-				int z = Random.Range (0, shots.Length);
+				int z = Random.Range(0, shots.Length);
 
 				// If the helicopter is within half of the attack distance, play the damage effect
 				if (distance < (attackDistance / 2))
 				{
 					if (z == 0)
-						Game.instance.playerScript.PlayaDamage (20);
+						Game.instance.playerScript.PlayaDamage(20);
 				}
 
 				// Play the audio clip at the randomized position
-				AudioSource.PlayClipAtPoint (shots [z], pos, 0.1f);
+				AudioSource.PlayClipAtPoint(shots[z], pos, 0.1f);
 
 				// Call the Fire method for both guns
-				Fire (pos, guns[0]);
-				Fire (pos, guns[1]);
+				Fire(pos, guns[0]);
+				Fire(pos, guns[1]);
 			}
 
 			// Wait for the fire rate duration before checking for shooting again
-			yield return new WaitForSeconds (fireRate);
+			yield return new WaitForSeconds(fireRate);
 		}
 	}
 
-	
+
 	// Enemy A.I. Loop
-	private IEnumerator UpdateAI () 
+	private IEnumerator UpdateAI()
 	{
-	    // wait for 10 seconds before starting
-	    yield return new WaitForSeconds (10);
+		// wait for 10 seconds before starting
+		yield return new WaitForSeconds(10);
 
-	    // repeat indefinitely
-	    while (true)
-	    {
-		// if helicopter is falling, stop shooting and exit coroutine
-		if (falling)
+		// repeat indefinitely
+		while (true)
 		{
-		    shooting = false;
-		    yield break;
-		}
+			// if helicopter is falling, stop shooting and exit coroutine
+			if (falling)
+			{
+				shooting = false;
+				yield break;
+			}
 
-		// calculate the distance between helicopter and target
-		float distance = Vector3.Distance (transform.position, target.position);
+			// calculate the distance between helicopter and target
+			float distance = Vector3.Distance(transform.position, target.position);
 
-		// set target height based on target's y position and current height
-		targetHeight = target.position.y + currentHeight;
+			// set target height based on target's y position and current height
+			targetHeight = target.position.y + currentHeight;
 
-		// perform linecast between helicopter and target
-		if (Physics.Linecast (transform.position, target.position, out RaycastHit hit2))
-		{
-		    // if linecast hits an object with "Calculator" tag and distance is less than attack distance
-		    if (hit2.collider.CompareTag ("Calculator"))
-		    {
-			if (distance < attackDistance)
-			    // set shooting flag to true
-			    shooting = true;
+			// perform linecast between helicopter and target
+			if (Physics.Linecast(transform.position, target.position, out RaycastHit hit2))
+			{
+				// if linecast hits an object with "Calculator" tag and distance is less than attack distance
+				if (hit2.collider.CompareTag("Calculator"))
+				{
+					if (distance < attackDistance)
+						// set shooting flag to true
+						shooting = true;
+					else
+						// set shooting flag to false
+						shooting = false;
+				}
+				else
+					// set shooting flag to false
+					shooting = false;
+			}
 			else
-			    // set shooting flag to false
-			    shooting = false;
-		    } 
-		    else
-			// set shooting flag to false
-			shooting = false;
-		} 
-		else
-		    // set shooting flag to false
-		    shooting = false;
+				// set shooting flag to false
+				shooting = false;
 
-		// set target position based on target's x, y, and z positions and target height
-		Vector3 targetPos = new Vector3 (target.position.x, targetHeight, target.position.z);
-		// smoothly move helicopter towards target position
-		transform.position = Vector3.Lerp (transform.position, targetPos, speed * Time.deltaTime);
+			// set target position based on target's x, y, and z positions and target height
+			Vector3 targetPos = new Vector3(target.position.x, targetHeight, target.position.z);
+			// smoothly move helicopter towards target position
+			transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
 
-		// calculate relative position between helicopter and target
-		Vector3 relativePos = target.position - transform.position;
-		// get rotation to look at target
-		Quaternion LookAtRotation = Quaternion.LookRotation (relativePos);
+			// calculate relative position between helicopter and target
+			Vector3 relativePos = target.position - transform.position;
+			// get rotation to look at target
+			Quaternion LookAtRotation = Quaternion.LookRotation(relativePos);
 
-		// clamp x rotation between 0 and 30 degrees
-		float x = LookAtRotation.eulerAngles.x;
-		x = Mathf.Clamp (x, 0, 30);
+			// clamp x rotation between 0 and 30 degrees
+			float x = LookAtRotation.eulerAngles.x;
+			x = Mathf.Clamp(x, 0, 30);
 
-		// clamp z rotation between -30 and 30 degrees
-		float z = LookAtRotation.eulerAngles.z;
-		x = Mathf.Clamp (x, -30, 30);
+			// clamp z rotation between -30 and 30 degrees
+			float z = LookAtRotation.eulerAngles.z;
+			x = Mathf.Clamp(x, -30, 30);
 
-		// get rotation only in y axis with clamped x and z rotations
-		Quaternion LookAtRotationOnly_Y = Quaternion.Euler (x, LookAtRotation.eulerAngles.y + offset, z);
+			// get rotation only in y axis with clamped x and z rotations
+			Quaternion LookAtRotationOnly_Y = Quaternion.Euler(x, LookAtRotation.eulerAngles.y + offset, z);
 
-		// smoothly rotate helicopter towards target
-		transform.rotation = Quaternion.Slerp (transform.rotation, LookAtRotationOnly_Y, turnSpeed * Time.deltaTime);
+			// smoothly rotate helicopter towards target
+			transform.rotation = Quaternion.Slerp(transform.rotation, LookAtRotationOnly_Y, turnSpeed * Time.deltaTime);
 
-		// wait for next frame before repeating
-		yield return null;
-	    }
+			// wait for next frame before repeating
+			yield return null;
+		}
 	}
-    	#endregion
+	#endregion
 }
